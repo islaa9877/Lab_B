1ae260f8ad2ff38cb4ee57cfc6c0b14e49b377c0
adnanislam73 <105314448+adnanislam73@users.noreply.github.com>
2022-11-08 21:29:38 -0500

    first commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+/* doesn't work with source level 1.8:
+module adnan.tutorial_b {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    opens adnan.tutorial_b to javafx.fxml;
+    exports adnan.tutorial_b;
+}
+*/
